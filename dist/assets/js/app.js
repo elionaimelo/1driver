01960888e844b7416af1076ef3b066db4638029f
Elionai Melo <elionai.melo@lais.huol.ufrn.br>
2020-07-01 14:17:49 -0300

    fix close [Fale conosco] Form #13

--- dist/assets/js/app.js
+++ dist/assets/js/app.js
@@ -1,3 +1,10 @@
 function goBack() {
   window.history.back();
 }
+//textarea com material design e altura auto ajustável
+// $('textarea').each(function () {
+//   this.setAttribute('style', 'height:' + (this.scrollHeight-10) + 'px;overflow-y:hidden;');
+// }).on('input', function () {
+//   this.style.height = 'auto';
+//   this.style.height = (this.scrollHeight-10) + 'px';
+// });
