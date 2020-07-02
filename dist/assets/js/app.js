30ba92eeefa86d01a96092b3527b9842947fb3b5
Elionai Melo <elionai.melo@lais.huol.ufrn.br>
2020-07-02 11:14:57 -0300

    ajustes no mostrar e esconder da senha

--- dist/assets/js/app.js
+++ dist/assets/js/app.js
@@ -22,4 +22,16 @@ $(document).ready(function() {
           $('#show_hide_password i').addClass( "fa-eye" );
       }
   });
+  $("#show_hide_password1 a").on('click', function(event) {
+    event.preventDefault();
+    if($('#show_hide_password1 input').attr("type") == "text"){
+        $('#show_hide_password1 input').attr('type', 'password');
+        $('#show_hide_password1 i').addClass( "fa-eye-slash" );
+        $('#show_hide_password1 i').removeClass( "fa-eye" );
+    }else if($('#show_hide_password1 input').attr("type") == "password"){
+        $('#show_hide_password1 input').attr('type', 'text');
+        $('#show_hide_password1 i').removeClass( "fa-eye-slash" );
+        $('#show_hide_password1 i').addClass( "fa-eye" );
+    }
+});
 });
